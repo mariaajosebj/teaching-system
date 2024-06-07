f2b1298ae761863807d206ebde2f47be1aabe4cb
jdencala <jdencalahuaya@gmail.com>
2024-06-07 11:36:24 -0500

    Add(I): Add Teaching System Logic

--- src/Main.java
+++ src/Main.java
@@ -24,15 +24,91 @@ public class Main {
 
             switch (option) {
                 case 1:
+                    Student student = createStudent();
+                    studentList.add(student);
                     break;
                 case 2:
+                    Teacher teacher = createTeacher();
+                    teacherList.add(teacher);
                     break;
                 case 3:
+                    Subject subject = createSubject(teacherList, studentList);
+                    subjectList.add(subject);
                     break;
                 case 4:
                     System.out.println("***** Exit System *******");
                     break;
             }
         } while(option != 5);
+    }
+
+    public static Student createStudent() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a name: ");
+        String name = scanner.next();
+        System.out.println("Please enter a LastName: ");
+        String lastName = scanner.next();
+        System.out.println("Please enter age:");
+        int age = scanner.nextInt();
+        System.out.println("Please enter studentCode:");
+        String studentCode = scanner.next();
+        System.out.println("Please enter number of semester:");
+        int noOfSemester = scanner.nextInt();
+        return new Student(name, lastName, age, studentCode, noOfSemester);
+    }
+
+    public static Teacher createTeacher() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a name: ");
+        String name = scanner.next();
+        System.out.println("Please enter a LastName: ");
+        String lastName = scanner.next();
+        System.out.println("Please enter age:");
+        int age = scanner.nextInt();
+        System.out.println("Please enter teacherCode:");
+        String teacherCode = scanner.next();
+        System.out.println("Please enter years of Experience:");
+        int yearsOfExperience = scanner.nextInt();
+        return new Teacher(name, lastName, age, teacherCode, yearsOfExperience);
+    }
+
+    public static void printTeacherList(List<Teacher> teacherList) {
+        int index = 1;
+        for(Teacher teacher : teacherList) {
+            System.out.println(index + " . " + teacher.getName().toUpperCase()
+                    + " - " +  teacher.getLastName().toUpperCase());
+            index = index + 1;
+        }
+    }
+
+    public static void printStudentList(List<Student> studentList) {
+        int index = 1;
+        for(Student student : studentList) {
+            System.out.println(index + " . " + student.getName().toUpperCase()
+                    + " - " +  student.getLastName().toUpperCase());
+            index = index + 1;
+        }
+    }
+
+    public static Subject createSubject(List<Teacher> teacherList, List<Student> studentList) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a name: ");
+        String name = scanner.next();
+
+        System.out.println("Please select a teacher: ");
+        printTeacherList(teacherList);
+        int teacherOption = scanner.nextInt();
+        Teacher teacher = teacherList.get(teacherOption - 1);
+
+        List<Student> students = new ArrayList<>();
+
+        System.out.println("Please select a student: ");
+        printStudentList(studentList);
+        int studentOption = scanner.nextInt();
+        Student student = studentList.get(studentOption - 1);
+
+        students.add(student);
+
+        return new Subject(name, teacher, students);
     }
 }
